9690c030c55eb6a4226b3fee2767089698cc7ac6
Laura Van Poelvoorde <Laura.VanPoelvoorde@sciensano.be>
2018-10-25 14:34:45 +0200

    added .gitignore and script utils.py

---
+++ scripts/utils.py
@@ -0,0 +1,7 @@
+def print_hello():
+    """This function provides me a hello"""
+    print("hello")
+               
+def print_goodbey():
+    """This function provides me a hello"""
+    print("goodbey")
